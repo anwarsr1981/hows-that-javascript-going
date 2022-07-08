3616dbfe73fefb9d75d65a4f76deda16f125326d
anwarsr1981 <anwar.parrish@gmail.com>
2022-07-08 06:29:27 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var button = document.querySelector(".got-this");
+var modal = document.querySelector(".modal");
+var modalX = document.querySelector(".modal-x");
+
+var closeModal = function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+};
+
+// Bonus Step
+// var openModal = function () {
+//   modal.classList.add("show-modal");
+//   button.innerText = "You've got this!!!";
+// };
+
+button.addEventListener("click", function () {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!!";
+});
+
+modalX.addEventListener("click", function () {
+  closeModal();
+});
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+  if (e.key === "Escape") {
+    if (modal.classList.contains("show-modal")) {
+      closeModal();
+    }
+  }
+});
